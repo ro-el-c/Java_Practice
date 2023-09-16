4cf78d7bd1e56f56d4ba429ba799c4c5ac64cb7d
ro-el-c <chung3286@naver.com>
2023-09-16 20:15:35 +0900

    feat: 스트림 중간/최종 처리

---
+++ src/main/java/Student.java
@@ -0,0 +1,17 @@
+class Student {
+    String name;
+    Long score;
+
+    public Student(String name, Long score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+}
