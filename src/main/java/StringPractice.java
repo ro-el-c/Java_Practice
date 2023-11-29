0eddd98e44853c00c68fd801158c6c59b0bed7cf
ro-el-c <chung3286@naver.com>
2023-11-29 21:47:58 +0900

    feat: 문자열 isEmpty vs isBlank

---
+++ src/main/java/StringPractice.java
@@ -0,0 +1,13 @@
+public class StringPractice {
+    public static void main(String[] args) {
+        String noneString = "";
+        String withBlank = " ";
+
+        System.out.println("noneString.isEmpty() = " + noneString.isEmpty());
+        System.out.println("withBlank.isBlank() = " + noneString.isBlank());
+
+        System.out.println("withBlank.isEmpty() = " + withBlank.isEmpty());
+        System.out.println("withBlank.isBlank() = " + withBlank.isBlank());
+
+    }
+}
